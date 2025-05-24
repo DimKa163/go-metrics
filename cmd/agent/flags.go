34bfb2d2763f7a829016be3f9b1b22baacd4b6fc
DimKa163 <mitya.martynov.96@gmail.com>
2025-05-25 00:45:12 +0400

    fix: client

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -7,7 +7,7 @@ var reportInterval int
 var pollInterval int
 
 func parseFlags() {
-	flag.StringVar(&addr, "a", "http://localhost:8080/", "agent address")
+	flag.StringVar(&addr, "a", "http://localhost:8080", "agent address")
 	flag.IntVar(&reportInterval, "r", 10, "report interval in seconds")
 	flag.IntVar(&pollInterval, "p", 2, "poll interval in seconds")
 	flag.Parse()
