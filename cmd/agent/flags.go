0d837c49329b9c699350b5029bdc4e2c8021194b
DimKa163 <mitya.martynov.96@gmail.com>
2025-05-26 22:37:57 +0400

    fix: return previous def value for address

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -11,7 +11,7 @@ var reportInterval int
 var pollInterval int
 
 func parseFlags() {
-	flag.StringVar(&addr, "a", "localhost:8081", "agent address")
+	flag.StringVar(&addr, "a", "localhost:8080", "agent address")
 	flag.IntVar(&reportInterval, "r", 10, "report interval in seconds")
 	flag.IntVar(&pollInterval, "p", 2, "poll interval in seconds")
 	flag.Parse()
